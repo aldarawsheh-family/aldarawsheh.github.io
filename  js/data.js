5c12021503555d0a402eacd8427cfe45e52d3ecf
aldarawsheh-family <aaaaa500100015@gmail.com>
2026-05-01 10:30:43 +0300

    js/data.js

---
+++  js/data.js
@@ -0,0 +1,144 @@
+// js/data.js - بيانات شجرة الدرواشة الكاملة
+var LOCAL_DATA = {
+  name: "عليان",
+  displayName: "عليان",
+  birth: "1200",
+  status: "متوفى رحمه الله",
+  color: "#8B5A2B",
+  isRoot: true,
+  branchCode: "Z",
+  customShape: "default",
+  familyColor: "",
+  children: [
+    {
+      name: "هزيم العليان",
+      displayName: "هزيم",
+      birth: "1230",
+      status: "متوفى رحمه الله",
+      color: "#8B5A2B",
+      branchCode: "Z",
+      customShape: "default",
+      familyColor: "",
+      children: [
+        {
+          name: "عطشان الهزيم",
+          displayName: "عطشان",
+          birth: "1260",
+          status: "متوفى رحمه الله",
+          color: "#8B5A2B",
+          branchCode: "Z",
+          customShape: "default",
+          familyColor: "",
+          children: [
+            {
+              name: "درويش العلياني",
+              displayName: "درويش",
+              birth: "1280",
+              status: "متوفى رحمه الله",
+              color: "color-root",
+              branchCode: "D",
+              customShape: "default",
+              familyColor: "",
+              children: [
+                {
+                  name: "خليف الدرويش",
+                  displayName: "خليف",
+                  birth: "1320",
+                  status: "متوفى رحمه الله",
+                  color: "color-khulaif",
+                  branchCode: "H",
+                  customShape: "default",
+                  familyColor: "",
+                  children: [
+                    {
+                      name: "محمد الخليف",
+                      displayName: "محمد",
+                      birth: "1360",
+                      status: "متوفى رحمه الله",
+                      branchCode: "H",
+                      customShape: "default",
+                      familyColor: "",
+                      children: [
+                        {
+                          name: "ذياب المحمد",
+                          displayName: "ذياب",
+                          birth: "1390",
+                          status: "حي أطال الله بعمره",
+                          branchCode: "H",
+                          customShape: "default",
+                          familyColor: "",
+                          children: [
+                            { name: "محمد الذياب", displayName: "محمد", birth: "1420", status: "حي أطال الله بعمره", branchCode: "H", customShape: "default", familyColor: "" },
+                            { name: "أحمد الذياب", displayName: "أحمد", birth: "1425", status: "حي أطال الله بعمره", branchCode: "H", customShape: "default", familyColor: "" },
+                            { name: "خالد الذياب", displayName: "خالد", birth: "1430", status: "حي أطال الله بعمره", branchCode: "H", customShape: "default", familyColor: "" },
+                            { name: "سيار الذياب", displayName: "سيار", birth: "1435", status: "حي أطال الله بعمره", branchCode: "H", customShape: "default", familyColor: "" }
+                          ]
+                        }
+                      ]
+                    },
+                    { name: "حميد الخليف", displayName: "حميد", birth: "1365", status: "متوفى رحمه الله", branchCode: "H", customShape: "default", familyColor: "" },
+                    { name: "سليمه الخليف", displayName: "سليمه", birth: "1370", status: "متوفاه رحمها الله", branchCode: "H", customShape: "default", familyColor: "" }
+                  ]
+                },
+                {
+                  name: "علوش الدرويش",
+                  displayName: "علوش",
+                  birth: "1330",
+                  status: "متوفى رحمه الله",
+                  color: "color-aloosh",
+                  branchCode: "A",
+                  customShape: "default",
+                  familyColor: "",
+                  children: []
+                },
+                {
+                  name: "محيميد الدرويش",
+                  displayName: "محيميد",
+                  birth: "1335",
+                  status: "متوفى رحمه الله",
+                  color: "color-hamid",
+                  branchCode: "M",
+                  customShape: "default",
+                  familyColor: "",
+                  children: []
+                },
+                {
+                  name: "حميدو الدرويش",
+                  displayName: "حميدو",
+                  birth: "1340",
+                  status: "متوفى رحمه الله",
+                  color: "color-hamido",
+                  branchCode: "E",
+                  customShape: "default",
+                  familyColor: "",
+                  children: []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: "هملان الهزيم",
+          displayName: "هملان",
+          birth: "1265",
+          status: "متوفى رحمه الله",
+          color: "#8B5A2B",
+          branchCode: "Z",
+          customShape: "default",
+          familyColor: "",
+          children: [
+            {
+              name: "عبلان الهملان",
+              displayName: "عبلان",
+              birth: "1290",
+              status: "متوفى رحمه الله",
+              branchCode: "Z",
+              customShape: "default",
+              familyColor: ""
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
